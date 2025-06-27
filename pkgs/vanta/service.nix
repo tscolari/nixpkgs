2e6ee092086e67fd3b7cb424792a35b94caf3391
Tiago Scolari <tiago@diagrid.io>
2025-06-27 14:24:28 +0100

    Make vanta service more resilient

--- pkgs/vanta/service.nix
+++ pkgs/vanta/service.nix
@@ -19,20 +19,30 @@ in
     environment.systemPackages = [ cfg.package ];
 
     # Vanta doesn't recognize NIXos, so we pretend to be Ubuntu.
-    environment.etc."ubuntu-os-release".text = ''
-      NAME="Ubuntu"
-      VERSION="24.04 LTS (Noble Numbat)"
-      ID=ubuntu
-      ID_LIKE=debian
-      PRETTY_NAME="Ubuntu 24.04 LTS"
-      VERSION_ID="24.04"
-      HOME_URL="https://www.ubuntu.com/"
-      SUPPORT_URL="https://help.ubuntu.com/"
-      BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
-      PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
-      VERSION_CODENAME=noble
-      UBUNTU_CODENAME=noble
-    '';
+    environment.etc = {
+      "ubuntu-os-release".text = ''
+        NAME="Ubuntu"
+        VERSION="24.04 LTS (Noble Numbat)"
+        ID=ubuntu
+        ID_LIKE=debian
+        PRETTY_NAME="Ubuntu 24.04 LTS"
+        VERSION_ID="24.04"
+        HOME_URL="https://www.ubuntu.com/"
+        SUPPORT_URL="https://help.ubuntu.com/"
+        BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
+        PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
+        VERSION_CODENAME=noble
+        UBUNTU_CODENAME=noble
+      '';
+
+       # Also create lsb-release for older detection methods
+      "ubuntu-lsb-release".text = ''
+        DISTRIB_ID=Ubuntu
+        DISTRIB_RELEASE=24.04
+        DISTRIB_CODENAME=noble
+        DISTRIB_DESCRIPTION="Ubuntu 24.04 LTS"
+      '';
+    };
 
     systemd.packages = [ cfg.package ];
 
@@ -45,11 +55,16 @@ in
     systemd.services.vanta = {
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
+        Environment = [
+          "OSQUERY_HASH_CACHE_MAX=104857600"
+          "UDEV_BUFFER_SIZE=16777216"
+        ];
         PrivateTmp = true;
         NoNewPrivileges = true;
         RestrictNamespaces = true;
         RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6 AF_NETLINK";
-        CapabilityBoundingSet = "CAP_DAC_OVERRIDE CAP_SYS_ADMIN CAP_WRITE CAP_CREATE CAP_FOWNER CAP_CHOWN";
+        CapabilityBoundingSet = "CAP_DAC_OVERRIDE CAP_SYS_ADMIN CAP_SETUID CAP_SETGID CAP_FOWNER CAP_CHOWN CAP_NET_ADMIN";
+        AmbientCapabilities = "CAP_SETUID CAP_SETGID";
         ProtectSystem = "strict";
         PrivateDevices = true;
         ProtectKernelModules = true;
@@ -69,6 +84,11 @@ in
         RestrictRealtime = true;
         RestrictSUIDSGID = true;
 
+        Restart = "on-failure";
+        RestartSec = "30s";
+        StartLimitBurst = 3;
+        StartLimitIntervalSec = "5min";
+
         ReadWritePaths = [
           "/var/vanta"
           "/var/vanta/log"
@@ -86,6 +106,7 @@ in
           "${cfg.package}/var/vanta/osqueryd:/var/vanta/osqueryd:norbind"
           "${cfg.package}/var/vanta/osquery-vanta.ext:/var/vanta/osquery-vanta.ext:norbind"
           "/etc/ubuntu-os-release:/etc/os-release"
+          "/etc/ubuntu-lsb-release:/etc/lsb-release"
         ];
       };
     };
